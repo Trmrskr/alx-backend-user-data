7cac130fbd440a56b46e234ddf8ffab3b59d74cf
Trmrskr <simonalale@gmail.com>
2023-08-16 15:36:29 +0000

    pep8 correction

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class User(Base):
     """Representation of Users"""
     __tablename__ = 'users'
